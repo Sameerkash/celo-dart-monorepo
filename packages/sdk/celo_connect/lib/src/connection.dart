00b04e0e8ba2a51cfa611b94c3c1f1fff78fe979
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2021-08-06 16:17:44 +0530

    basic connection to figment rpc

--- packages/sdk/celo_connect/lib/src/connection.dart
+++ packages/sdk/celo_connect/lib/src/connection.dart
@@ -1,10 +1,24 @@
-import 'types.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
 
-abstract class ConnectionOptions {
-  late int gasInflationFactor;
-  late String gasPrice;
-  Address? feeCurrency;
-  Address? address;
+late Credentials credentials;
+late Web3Client ethClient;
+
+Future<void> initializeCLient(
+    {required String rpcUrl, required String privateKey}) async {
+  final httpClient = Client();
+  ethClient = Web3Client(rpcUrl, httpClient);
+
+  credentials = await ethClient.credentialsFromPrivateKey(privateKey);
 }
 
-
+Future<void> getUserAccount() async {
+  final myAddress = await credentials.extractAddress();
+  final balance = await ethClient.getBalance(myAddress);
+  final gasPrice = await ethClient.getGasPrice();
+  final tx = await ethClient.getTransactionCount(myAddress);
+  print(myAddress);
+  print(balance);
+  print(gasPrice);
+  print(tx);
+}
